0b654dd629551a7c1bdec2a4355964223b1031ac
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-05-30 09:27:42 +0900

    Initial Commit

---
+++ YachtDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YachtDice
+//
+//  Created by 최승범 on 5/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
